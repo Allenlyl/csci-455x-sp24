19c941efed19c23fcbe485f568d348d55a2f73aa
Allen Liang <yilangliang@wustl.edu>
2024-02-05 01:24:29 -0800

    Start Bookshelf.java, BookshelfKeeper.java and BookshelfTester.java

---
+++ project assignment/pa2/TestAssert.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestAssert {
+    public static void main(String[] args) {
+        ArrayList<Integer> books1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        Bookshelf bookshelf1 = new Bookshelf(books1);
+
+        ArrayList<Integer> books2 = new ArrayList<>(Arrays.asList(1, -10, 3, -3, 5));
+        Bookshelf bookshelf2 = new Bookshelf(books2);
+    }
+}
